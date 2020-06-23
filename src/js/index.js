476a9f5593a72b7f6c4ae2aaad33133a16a75b58
Piotr <piotr-gorka@wp.pl>
2020-06-23 20:07:47 +0100

    background color and counting buttons

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,29 @@
-import '../scss/main.scss';
+import "../scss/main.scss";
 
 // uncomment the lines below to enable PWA
-// import {registerSW} from './pwa.js';
-// registerSW();
+import { registerSW } from "./pwa.js";
+registerSW();
 
 /* place your code below */
 
-console.log('HELLO 🚀')
+console.log("HELLO 🚀");
+
+const add = document.querySelector(".add--js");
+const remove = document.querySelector(".remove--js");
+let counter = document.querySelector(".counter--js");
+
+let number = 0;
+
+add.addEventListener("click", () => {
+  number++;
+  counter.innerHTML = number;
+});
+
+remove.addEventListener("click", () => {
+  if (number > 0) {
+    number--;
+    counter.innerHTML = number;
+  }else if(number <= 0){
+      counter.innerHTML = 0;
+  }
+});
