ee1cb1463cfa63ee84eb4f312da197bab39547d5
Piotr <piotr-gorka@wp.pl>
2020-06-24 20:12:02 +0100

    working localStorage

--- src/js/index.js
+++ src/js/index.js
@@ -4,25 +4,43 @@ import "../scss/main.scss";
 import { registerSW } from "./pwa.js";
 registerSW();
 
-
 const add = document.querySelector(".add--js");
 const remove = document.querySelector(".remove--js");
 let counter = document.querySelector(".counter--js");
 
-let number = 0;
 
-const key = new Date().toISOString().slice(0, 10)
+const key = new Date().toISOString().slice(0, 10);
+const entry = localStorage.getItem(key);
+
+
+let result = "";
+
+/////////////////////////
+
+if (entry) {
+  result = entry;
+}
+
+counter.innerHTML = result;
+ let number = result;
+
+
+//console.log(result);
+//console.log(entry);
+
 
 add.addEventListener("click", () => {
   number++;
   counter.innerHTML = number;
+  localStorage.setItem(key, number);
 });
 
 remove.addEventListener("click", () => {
   if (number > 0) {
     number--;
     counter.innerHTML = number;
-  }else if(number <= 0){
-      counter.innerHTML = 0;
+    localStorage.setItem(key, number);
+  } else if (number <= 0) {
+    counter.innerHTML = 0;
   }
 });
